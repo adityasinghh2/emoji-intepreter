f264a09be753d134446c263e0c0388b12ac2ecbd
Aditya singh <adityasinghh020@gmail.com>
2021-12-21 17:51:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+// var headingText = "inside out";
+// var color = "blue";
+
+var username = prompt("Give me your name");
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={{ backgroundColor: "blue" }}>welcome{username} </h1>
+    </div>
+  );
+}
+
+//things to notice
+//  class --->  classname
+// style ---> takes an object instead of***
