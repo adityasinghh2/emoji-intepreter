e729be47864e11af871310ab24f8e86e4e11e792
Aditya singh <adityasinghh020@gmail.com>
2021-12-22 00:15:00 +0530

    used useState function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 // var headingText = "inside out";
 // var color = "blue";
 
-var username = prompt("Give me your name");
+// var username = prompt("Give me your name");
 
+// var username = "Aditya";
+
+// var likeCounter = 0;
 export default function App() {
+  const [likeCounter, setLikeCounter] = useState(0);
+
+  function likeClickHandler() {
+    var newLikeCountervalue = likeCounter + 1;
+    // likeCounter = likeCounter + 1; //processing
+    // console.log("clicked!", likeCounter); //output
+    setLikeCounter(newLikeCountervalue);
+  }
+
   return (
     <div className="App">
-      <h1 style={{ backgroundColor: "blue" }}>welcome {username} </h1>
+      <h1>inside out!</h1>
+      <button onClick={likeClickHandler}> Like Me! </button> {likeCounter}
     </div>
   );
 }
-
-//things to notice
-//  class --->  classname
-// style ---> takes an object instead of***
